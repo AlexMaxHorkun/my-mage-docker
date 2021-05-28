f2a4e7cc7e733834a4b411397fde4c0603ac12b2
ogorkun <ogorkun@adobe.com>
2021-05-28 10:56:05 -0500

    Updated elastic

--- MagentoDocker/install/elasticsearch.sh
+++ MagentoDocker/install/elasticsearch.sh
@@ -3,8 +3,8 @@ bin/echo -e "\n\n\e[1;32m=======================================================
 bin/echo -e "\e[1;32mInstalling Elasticsearch\e[0m"
 bin/echo -e "\n\n\e[1;32m=====================================================================================\e[0m\n\n"
 
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.8.0-amd64.deb
-sudo dpkg -i elasticsearch-7.8.0-amd64.deb
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.10.2-amd64.deb
+sudo dpkg -i elasticsearch-7.10.2-amd64.deb
 
 bin/echo -e "\n\n\e[1;32m=====================================================================================\e[0m\n\n"
 bin/echo -e "\e[1;32mInstalled Elasticsearch\e[0m"
