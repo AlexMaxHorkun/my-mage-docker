70dd3f766ab3b2c124892d10a460eebdf03d093c
ogorkun <ogorkun@adobe.com>
2021-06-16 11:05:30 -0500

    updates for kubernetes

--- MagentoDocker/init/restart_magento.sh
+++ MagentoDocker/init/restart_magento.sh
@@ -5,3 +5,6 @@ service php7.3-fpm stop
 service php7.4-fpm start ||\
 service php7.3-fpm start
 service nginx start
+echo "Restarted services"
+
+return 0;
--- MagentoDocker/install/mftf_requirements.sh
+++ MagentoDocker/install/mftf_requirements.sh
@@ -5,8 +5,9 @@ cd /etc/magento/system &&\
 wget https://dl.google.com/linux/linux_signing_key.pub &&\
 apt install -yqf gpg-agent &&\
 apt-key add linux_signing_key.pub &&\
-apt update &&\
-apt install -yqf google-chrome-stable &&\
+apt update
+apt install -yqf google-chrome-stable ||\
+apt install -yqf chromium-browser
 chmod +x /etc/magento/resources/mftf/chromedriver &&\
 ln -s /etc/magento/resources/mftf/chromedriver /usr/bin/chromedriver &&\
 mkdir /etc/magento/mftf && mkdir /etc/magento/mftf/logs
